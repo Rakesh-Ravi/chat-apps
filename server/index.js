3ea584a161ff3e9db846a43d8a8ef6bddf828a21
Rakesh-Ravi <rakeshravi6300@gmail.com>
2024-01-31 21:07:35 +0530

    first commit

---
+++ server/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+
+const app = express();
+require("dotenv").config();
+
+
+app.use(cors({
+    origin: 'http://localhost:3000', 
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  }))
+
+  console.log('CORS middleware configured');
+app.use(express.json());
+
+app.use("/api/auth",userRoutes);
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}) .then(()=>{
+    console.log("DB Connection Successful");
+}).catch((err)=>{
+    console.log(err.message);
+})
+
+const server = app.listen(process.env.PORT,()=>{
+    console.log(`Server Started on Port ${process.env.PORT}`);
+})
